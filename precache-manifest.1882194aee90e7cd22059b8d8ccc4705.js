4360f389eeb62de10a575809c85c30f40de57fa5
Touhidul Shawan <touhidulshawan@gmail.com>
2020-09-16 22:15:47 +0600

    Updates

---
+++ precache-manifest.1882194aee90e7cd22059b8d8ccc4705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf61a5c49484e4d948f6ee5cbbac1073",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "5389e36f56e3b7272418",
+    "url": "/simple-todo/static/css/main.ec724898.chunk.css"
+  },
+  {
+    "revision": "2ce53f0e6b3e7d2af6f1",
+    "url": "/simple-todo/static/js/2.0c131fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo/static/js/2.0c131fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5389e36f56e3b7272418",
+    "url": "/simple-todo/static/js/main.f101a065.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
